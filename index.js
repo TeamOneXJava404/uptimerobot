14ce62ab0bd28d36f7ed724ffd949875a0905ee9
TeamOneXJava404 <67852264+TeamOneXJava404@users.noreply.github.com>
2020-07-21 10:21:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://pretty-kunztol.glitch.me"]
+const urls = ["https://pretty-chan.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
